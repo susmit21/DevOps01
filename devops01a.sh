4e94107125fab61330a4c368a085b977f62723f7
Susmit Mogarkar <susmit.mogarkar@gmail.com>
2019-05-27 13:33:42 -0700

    Added command to display Number of volumes, Size of each volume, Free space on each volume

--- devops01a.sh
+++ devops01a.sh
@@ -6,6 +6,8 @@ get_system_data()
 printf "############# Number of volumes ###############\n\n" >> sysinfo
 diskutil list >> sysinfo
 
+printf "############ Number of volumes, Size of each volume, Free space on each volume  #############\n\n" >> sysinfo
+df -h >> sysinfo
 
 }
 
