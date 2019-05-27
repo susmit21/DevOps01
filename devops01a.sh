735d586b0d67175ba0550822dca092b746f7538b
Susmit Mogarkar <susmit.mogarkar@gmail.com>
2019-05-27 13:36:23 -0700

    Added command to display Number of CPUs

--- devops01a.sh
+++ devops01a.sh
@@ -9,6 +9,9 @@ diskutil list >> sysinfo
 printf "############ Number of volumes, Size of each volume, Free space on each volume  #############\n\n" >> sysinfo
 df -h >> sysinfo
 
+printf "############ Number of CPUs ################\n\n" >> sysinfo
+sysctl -n hw.ncpu >> sysinfo
+
 }
 
 get_system_data
